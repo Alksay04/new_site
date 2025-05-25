e7e3d7b85835b6dc158e7e340218c68c86e5a31a
Berserk883 <egorhyldikob571@gmail.com>
2025-05-25 15:19:12 +0800

    'pon'

--- js/questions.js
+++ js/questions.js
@@ -57,17 +57,41 @@ const questions = [
 ]
 
 var currentQuestion = 0;
+var answerGiven = false;
+var valor = 0;
+
+const nextButton = document.getElementById('next_button');
+const skipButton = document.getElementById('skip_button');
 
 function selectAnswer(event, correct_answer){
+    if (answerGiven)
+        return
+    answerGiven = true;
     const target = event.target;
 
-    if (correct_answer == target.getAttribute('data-answer-index'))
+    nextButton.classList.remove('hidden')
+    skipButton.classList.add('hidden')
+
+    if (correct_answer == target.getAttribute('data-answer-index')){
         target.classList.add('correct')
-    else
+        valor += 1
+    }
+    else {
         target.classList.add('wrong')
+        const correctAnswerDiv = document.getElementById('correct_answer')
+        correctAnswerDiv.classList.add('correct')
+    }
+        
 }
 
 function newQuestion(){
+    answerGiven = false
+
+    nextButton.classList.add('hidden')
+    skipButton.classList.remove('hidden')
+
+
+
     const questionsContainer = document.getElementById("question_container")
     const answerContainer = document.getElementById("answers_container")
 
@@ -88,11 +112,18 @@ function newQuestion(){
         newAnswer.setAttribute('data-answer-index', i)
         newAnswer.innerText = answer
         newAnswer.setAttribute('onclick', `selectAnswer(event, ${question.correct_answer})`)
+        if (i == question.correct_answer)
+            newAnswer.id = 'correct_answer'
         answerContainer.append(newAnswer)
     }
 }
 
 function skip(){
+    currentQuestion += 1
+    newQuestion()
+}
+
+function next(){
     currentQuestion += 1
     newQuestion()
 }
